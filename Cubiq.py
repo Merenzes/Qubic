be2144cc29f6da965f8defb30eeb20e8fa29ef0d
Michał Wójtowicz <m.wojtowicz@live.co.uk>
2018-11-27 08:15:42 +0100

    Added player movement in Game

--- Cubiq.py
+++ Cubiq.py
@@ -12,31 +12,61 @@ class Board:
         #print('top \n', self.board[0], '\n mid \n', self.board[1], '\n bottom \n', self.board[2])
         print('_'* 25)
     
-    def mark_cross(self, z, x, y):
+    def mark_P1(self, z, x, y):
         self.board[z,x,y] = 1
 
-    def mark_os(self, z, x, y):
-        self.board[z, x, y] = 2
+    def mark_P2(self, z, x, y):
+        self.board[z, x, y] = -1
+
+    def is_occpied(self, z, x ,y):
+        if self.board[z, x, y] == 1 or self.board[z, x, y] == -1:
+            return True
+        else:
+            return False
 
 class Game:
     game_board = Board()
     player_one = 'P1'
     player_two = 'P2'
 
-    #def __init__(self):
-        
+    def __init__(self):
+        self.game_loop()
     
-    #def game_loop():
-        #satrt game
-        #ask P1 to make move
-        #check for win
-        #ask P2 to make move
-        #check for win
-        #repeat above until one player win or there are no chance to win
+    def game_loop(self):
+        # start game
+        # loop start
+        # ask P1 to make move
+        self.player_move(self.player_one)
+        # check for win condition
+        # ask P2 to make move
+        self.player_move(self.player_two)
+        # check for win condition
+        # repeat above until one player win or there are no chance to win
+        # loop end
+        # display who win
+    
+    # ask player for coordinates to place his mark
+    # check if this spot is avilable
+    def ask_for_coordinates(self, player):
+        z, x, y = [int(x) for x in input(player + ', where do you want to place yoour marker? : ').split()]
+        while(z < 0 or z > 4 or x < 0 or x > 4 or y < 0 or y > 4 or self.game_board.is_occpied(z, x, y)):
+            print('Incorrect values or spot is occupied!')
+            z, x, y = [int(x) for x in input(player + ', where do you want to place yoour marker? : ').split()]
+        return z,x,y
+
+    def player_move(self, player):
+        if player == self.player_one:
+            z, x, y = self.ask_for_coordinates(player)
+            self.game_board.mark_P1(z, x, y)
+            self.game_board.print_board()
+        else:
+            z, x, y = self.ask_for_coordinates(player)
+            self.game_board.mark_P2(z, x, y)
+            self.game_board.print_board()
 
     def check_win(self):
         win = False
-        where = [0,0,0]        
+        where = [0,0,0]
         
         return win, where
 
@@ -48,11 +78,13 @@ class Game:
 #b.print_board()
 
 
-board = np.arange(64).reshape(4,4,4)
-print(board)
+#board = np.arange(64).reshape(4,4,4)
+#print(board)
 #print("up plane first column")
 #print(board[0,:,0])
 #print("[0,0] from 2 planes")
 #print(board[:,0,0])
 #s = np.all(board == board[:,0,0])
 #print(s)
+
+g = Game()
