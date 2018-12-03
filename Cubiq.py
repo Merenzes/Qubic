5c6791afe83d7a459e0cd417058617db6b97ad2c
Michał Wójtowicz <m.wojtowicz@live.co.uk>
2018-12-03 14:17:21 +0100

    sdfg

--- Cubiq.py
+++ Cubiq.py
@@ -24,8 +24,51 @@ class Board:
         else:
             return False
     
+    '''
+    All posible diagonals
+    top view left -> right  
+    [0, 0, 0] [0, 1, 1] [0, 2, 2] [0, 3, 3]
+    [1, 0, 0] [1, 1, 1] [1, 2, 2] [1, 3, 3]
+    [2, 0, 0] [2, 1, 1] [2, 2, 2] [2, 3, 3]
+    [3, 0, 0] [3, 1, 1] [3, 2, 2] [3, 3, 3]
+    top view right -> left
+    [0, 0, 3] [0, 1, 2] [0, 2, 1] [0, 3, 0]
+    [1, 0, 3] [1, 1, 2] [1, 2, 1] [1, 3, 0]
+    [2, 0, 3] [2, 1, 2] [2, 2, 1] [2, 3, 0]
+    [3, 0, 3] [3, 1, 2] [3, 2, 1] [3, 3, 0]
+    side view left -> right
+    [0, 0, 0] [1, 0, 1] [2, 0, 2] [3, 0, 3]
+    [0, 1, 0] [1, 1, 1] [2, 1, 2] [3, 1, 3]
+    [0, 2, 0] [1, 2, 1] [2, 2, 2] [3, 2, 3]
+    [0, 3, 0] [1, 3, 1] [2, 3, 2] [3, 3, 3]
+    side view right -> left
+    [0, 0, 3] [1, 0, 2] [2, 0, 1] [3, 0, 0]
+    [0, 1, 3] [1, 1, 2] [2, 1, 1] [3, 1, 0]
+    [0, 2, 3] [1, 2, 2] [2, 2, 1] [3, 2, 0]
+    [0, 3, 3] [1, 3, 2] [2, 3, 1] [3, 3, 0]
+    front view left -> right
+    [0, 0, 3] [1, 1, 3] [2, 2, 3] [3, 3, 3]
+    [0, 0, 2] [1, 1, 2] [2, 2, 2] [3, 3, 2]
+    [0, 0, 1] [1, 1, 1] [2, 2, 1] [3, 3, 1]
+    [0, 0, 0] [1, 1, 0] [2, 2, 0] [3, 3, 0]
+    front view right -> left
+    [0, 3, 3] [1, 2, 3] [2, 1, 3] [3, 0, 3]
+    [0, 3, 2] [1, 2, 2] [2, 1, 2] [3, 0, 2]
+    [0, 3, 1] [1, 2, 1] [2, 1, 1] [3, 0, 1]
+    [0, 3, 0] [1, 2, 0] [2, 1, 0] [3, 0, 0]
+    main diagonals
+    [0, 0, 0] [1, 1, 1] [2, 2, 2] [3, 3, 3]
+    [3, 0, 3] [2, 1, 2] [1, 2, 1] [0, 3, 0]
+    [3, 0, 0] [2, 1, 1] [1, 2, 2] [0, 3, 3]
+    [0, 0, 3] [1, 1, 2] [2, 2, 1] [3, 3, 0]
+    '''
     def win_condition(self):
         test_elements = [-4, 4]
+        #top - > bottom
+        for z in range(4):
+            for xy in range(4):
+                
+                
         if (
             np.any(np.isin(np.sum(self.board, axis = 0), test_elements)) or 
             np.any(np.isin(np.sum(self.board, axis = 1), test_elements)) or 
@@ -85,10 +128,17 @@ class Game:
             return False
 
     def game_AI(self):
-        for z in range(3):
-            for y in range(3):
-                for x in range(3):
+        for z in range(4):
+            for y in range(4):
+                for x in range(4):
                     if not self.game_board.is_occpied(z, x, y):
                         return z, x, y
 
-g = Game()
+board = np.zeros((4, 4, 4))
+board[0, 0, 0] = 1
+board[1, 1, 1] = 1
+board[2, 2, 2] = 1
+board[3, 3, 3] = 1
+
+print(np.trace(board,axis1=1, axis2=2))
+
